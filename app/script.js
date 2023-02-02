b2c856a499ac85cfa4a38a0371d3b4185b624bf9
olaf <olafer25227@gmail.com>
2023-02-02 13:05:00 +0100

    working on responsive navbar

--- app/script.js
+++ app/script.js
@@ -1,6 +1,12 @@
 "use strict";
 const form = document.querySelector("form");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".navBg");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+});
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("navActive");
 });
